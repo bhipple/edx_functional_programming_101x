9db3b806c5c8ca37754969383e785c3829139cda
Benjamin Hipple <brh2113@columbia.edu>
2015-11-26 12:52:46 -0500

    Finished lab4

--- lab4.hs
+++ lab4.hs
@@ -11,14 +11,19 @@ import Data.Char
 -- ===================================
 
 triangle :: Integer -> Integer
-triangle n = undefined
+triangle n
+    | n == 0 = 0
+    | otherwise = n + triangle (n-1)
 
 -- ===================================
 -- Ex. 1
 -- ===================================
 
 count :: Eq a => a -> [a] -> Int
-count x xs = undefined
+count _ [] = 0
+count y (x:xs)
+    | x == y = 1 + count y xs
+    | otherwise = count y xs
 
 xs = [1,2,35,2,3,4,8,2,9,0,5,2,8,4,9,1,9,7,3,9,2,0,5,2,7,6,92,8,3,6,1,9,2,4,8,7,1,2,8,0,4,5,2,3,6,2,3,9,8,4,7,1,4,0,1,8,4,1,2,4,56,7,2,98,3,5,28,4,0,12,4,6,8,1,9,4,8,62,3,71,0,3,8,10,2,4,7,12,9,0,3,47,1,0,23,4,8,1,20,5,7,29,3,5,68,23,5,6,3,4,98,1,0,2,3,8,1]
 ys = map (\x -> ((x + 1) * 3) ^ 3 - 7) xs
@@ -38,11 +43,15 @@ poem = [ "Three Types for the Lisp-kings under the parentheses,"
 -- ===================================
 
 euclid :: (Int,  Int) -> Int
-euclid (x, y) = undefined
+euclid (x, y)
+    | x == y = x
+    | x > y = euclid (x-y, y)
+    | otherwise = euclid (x, y-x)
 
 -- ===================================
 -- Ex. 3
 -- ===================================
 
 funkyMap :: (a -> b) -> (a -> b) -> [a] -> [b]
-funkyMap f g xs = undefined
+funkyMap _ _ [] = []
+funkyMap f g (x:xs) = ((f x) : funkyMap g f xs)
